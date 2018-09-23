c79d28f5d8770b57edd6e14eb8abb478e79791c2
Ahrar Monsur <ahrarmonsur93@gmail.com>
2018-09-22 20:10:01 -0400

    Adds the conversion result mock page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./routes/index');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 // Set up the Express webserver
@@ -11,6 +11,8 @@ const app = express();
 // Set the root directory for the views and public files
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 // Attach the routes to the webserver
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index.html');
+	res.render('index');
+});
+
+router.get('/conversion-result', function(req, res, next) {
+	res.render('conversion-result');
 });
 
 module.exports = router;
