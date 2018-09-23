4dde40377cd69fa95992af6713540c3f8d3b9b82
Ahrar Monsur <ahrarmonsur93@gmail.com>
2018-09-23 02:14:15 -0400

    Adds GCS credentials and successfully sends images to GCS for parsing

--- public/js/gogglevision.js
+++ public/js/gogglevision.js
@@ -5,24 +5,36 @@ const fs = require('fs');
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-const fileName = document.getElementById("img").value();
-fileName = fileName.replace('C:\\fakepath\\', '');
-const request = {
-  image: {
-    content: fs.readFileSync(uri),
-  },
-  feature: {
-    languageHints: ['en-t-i0-handwrit'],
-    type: ['TEXT_DETECTION'],
-  },
+const imageElement = document.getElementById("img");
+console.log(imageElement);
+
+
+imageElement.onchange = function () {
+	var file = imageElement.files[0];
+	console.log(file);
+	//upload(file);
+	//drawOnCanvas(file);   // see Example 6
+	//displayAsImage(file); // see Example 7
 };
 
-client
-  .documentTextDetection(request)
-  .then(results => {
-    const fullTextAnnotation = results[0].fullTextAnnotation;
-    console.log(`Full text: ${fullTextAnnotation.text}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+
+// fileName = fileName.replace('C:\\fakepath\\', '');
+// const request = {
+// 	image: {
+// 		content: fs.readFileSync(uri),
+// 	},
+// 	feature: {
+// 		languageHints: ['en-t-i0-handwrit'],
+// 		type: ['TEXT_DETECTION'],
+// 	},
+// };
+//
+// client
+// 	.documentTextDetection(request)
+// 	.then(results => {
+// 		const fullTextAnnotation = results[0].fullTextAnnotation;
+// 		console.log(`Full text: ${fullTextAnnotation.text}`);
+// 	})
+// 	.catch(err => {
+// 		console.error('ERROR:', err);
+// 	});
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,43 @@
+const fs = require('fs')
+const multer= require('multer');
 const express = require('express');
 const router = express.Router();
+const fileFunctions = require('../lib/fileFunctions')
 
-router.get('/', function(req, res, next) {
+// Set up the destination path and the custom filename for handwritten image uploads
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+	    cb(null, 'assets/file_uploads/');
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + '-' + file.originalname);
+	}
+});
+
+// This enables the backend to receive an image file from a POST request,
+// with the **name** attribute of the file being 'handwritten_image'
+const handwritten_upload = multer({ storage: storage }).single('handwritten_image');
+
+router.get('/', (req, res, next) => {
 	res.render('index');
 });
 
-router.get('/conversion-result', function(req, res, next) {
+router.get('/conversion-result',(req, res, next) => {
 	res.render('conversion-result');
 });
+
+function relayImageToGoogleVision(req, res, next) {
+	fileFunctions.uploadImageToGoogleVision(req.file.path).then(results => {
+		const fullTextAnnotation = results[0].fullTextAnnotation;
+		console.log(results);
+		console.log(`Full text: ${fullTextAnnotation.text}`);
+	}).catch(err => {
+		console.log(`Error: ${err}`);
+
+	});
+
+	res.end('dsafsdf');
+}
+router.post('/handwritten', handwritten_upload, relayImageToGoogleVision);
 
 module.exports = router;
